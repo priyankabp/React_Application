82a37d47a5a3f53291eabda28f90008a5577d856
Priyanka Phapale <pbphapale@gmail.com>
2018-05-12 23:49:05 -0400

    Adding Hot Module Replacement (HMR)
    React output changes without browser refresh using HMR

--- src/App.js
+++ src/App.js
@@ -4,15 +4,10 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const helloWorld = "Welcome to the Road to learn react";
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <h2>{helloWorld}</h2>
       </div>
     );
   }
